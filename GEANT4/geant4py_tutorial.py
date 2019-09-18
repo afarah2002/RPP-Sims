0e6f9cd835f5197212e985a66d86da96a77587ca
Albert Farah <arfarah2002@gmail.com>
2019-09-18 00:59:15 -0400

    Adding multiple volumes: initialize, specify location, create, and place

--- GEANT4/geant4py_tutorial.py
+++ GEANT4/geant4py_tutorial.py
@@ -3,6 +3,7 @@ import g4py.ezgeom
 from g4py.ezgeom import G4EzVolume
 import g4py.NISTmaterials
 import g4py.EMSTDpl
+import random
 import time
 
 
@@ -34,30 +35,44 @@ gRunManager.SetUserInitialization(exN03PL)
 air= gNistManager.FindOrBuildMaterial("G4_AIR")
 g4py.ezgeom.SetWorldMaterial(air)
 # dummy box
-detector_box=G4EzVolume("DetectorBox")
-detector_box.CreateBoxVolume(air, 20.*cm, 20.*cm, 40.*cm)
-detector_box.PlaceIt(G4ThreeVector(0.,0.,20.*cm))
+detector_box = G4EzVolume("DetectorBox") #i nitialize volume
+detector_box.CreateBoxVolume(air, 20.*cm, 20.*cm, 40.*cm)# create volume
+detector_boxPos = G4ThreeVector(0.,0.,20.*cm) # specify xyz location of volume
+detector_box.PlaceIt(detector_boxPos) # add volume to space
 # calorimeter placed inside the box
-cal= G4EzVolume("Calorimeter")
+cal= G4EzVolume("Calorimeter") #initialize volume
 nai= gNistManager.FindOrBuildMaterial("G4_SODIUM_IODIDE")
 au= G4Material.GetMaterial("G4_Au")
-# cal.CreateTubeVolume(au, 30., 10.*cm, 10.*mm)
-cal.CreateOrbVolume(au, 10.*cm)
-dd= 20.*cm
-for ical in range(-1, 2):
-	calPos= G4ThreeVector(dd*ical, 0., 0.)
-	cal.PlaceIt(calPos, ical+1, detector_box)
+
+orb = G4EzVolume("Orb") # initialize volume
+orbPos = G4ThreeVector(1., 1., 1.) # specify xyz location of volume
+orb.CreateOrbVolume(au, 10.*cm)# create volume
+orb.PlaceIt(orbPos) # add volume to space
+
+tube = G4EzVolume("Tube") # initialize volume
+tubePos = G4ThreeVector(-1., -1., -1.) # specify xyz location of volume
+tube.CreateTubeVolume(au, 30., 10.*cm, 30.*cm) # create volume
+tube.PlaceIt(tubePos) # add volume to space
+
 gRunManager.Initialize()
 
 angle = 0
-zoom = 1
 while True:
 	angle += 5.
-	zoom += .00005
+	# angle = float(random.randint(1, 101))
+	
 	gApplyUICommand("/vis/sceneHandler/create OGLSX OGLSX")
 	gApplyUICommand("/vis/viewer/create OGLSX oglsxviewer")
 	gApplyUICommand("/vis/drawVolume")
 	time.sleep(.05)
-	gApplyUICommand("/vis/viewer/zoom " + str(zoom))
+	# gApplyUICommand("/vis/viewer/zoom " + str(zoom))
 	gApplyUICommand("/vis/viewer/set/viewpointThetaPhi 0" + str(angle))
 	gApplyUICommand("/vis/viewer/update")
+
+# class GeomBuild(object):
+
+# 	def __init__(self):
+
+# 	def Box(self, material, length, width, height):
+
+# 	def Tube(self, outer_radius, inner_radius, length)
