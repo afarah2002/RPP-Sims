e46ae3194b3c057060a1f34bfea2fe2d18fd8520
Albert Farah <arfarah2002@gmail.com>
2019-09-18 01:54:57 -0400

    Streamlined construction, demos functions in geom_constructor.py

--- GEANT4/geant4py_tutorial.py
+++ GEANT4/geant4py_tutorial.py
@@ -1,3 +1,4 @@
+#----------imports----------#
 from Geant4 import *
 import g4py.ezgeom
 from g4py.ezgeom import G4EzVolume
@@ -6,24 +7,8 @@ import g4py.EMSTDpl
 import random
 import time
 
-
-
-# def ConstructGeom():
-# 	print "* Constructing geometry..."
-# 	g4py.ezgeom.Construct()
-# 	g4py.NISTmaterials.Construct()
-# 	# reset world material
-# 	air= G4Material.GetMaterial("G4_AIR")
-# 	g4py.ezgeom.SetWorldMaterial(air)
-
-# 	# a target box is placed
-# 	global target
-# 	target= G4EzVolume("Target")
-# 	au= G4Material.GetMaterial("G4_Au")
-# 	target.CreateTubeVolume(au, 0., 1.*cm, 1.*mm)
-# 	target.PlaceIt(G4ThreeVector(0.,0.,-10.*cm))
-
-# ConstructGeom()
+#----file imports--------#
+from geom_constructor import GeomConstructor 
 
 # from EZsim.EZgeom import G4EzVolume
 g4py.NISTmaterials.Construct()
@@ -35,24 +20,25 @@ gRunManager.SetUserInitialization(exN03PL)
 air= gNistManager.FindOrBuildMaterial("G4_AIR")
 g4py.ezgeom.SetWorldMaterial(air)
 # dummy box
-detector_box = G4EzVolume("DetectorBox") #i nitialize volume
-detector_box.CreateBoxVolume(air, 20.*cm, 20.*cm, 40.*cm)# create volume
-detector_boxPos = G4ThreeVector(0.,0.,20.*cm) # specify xyz location of volume
-detector_box.PlaceIt(detector_boxPos) # add volume to space
+GC = GeomConstructor()
+GC.ConstructBox("Detector Box", air, [0., 0., 0.], cm, [20., 20., 40.])
+
 # calorimeter placed inside the box
 cal= G4EzVolume("Calorimeter") #initialize volume
 nai= gNistManager.FindOrBuildMaterial("G4_SODIUM_IODIDE")
 au= G4Material.GetMaterial("G4_Au")
 
-orb = G4EzVolume("Orb") # initialize volume
-orbPos = G4ThreeVector(1., 1., 1.) # specify xyz location of volume
-orb.CreateOrbVolume(au, 10.*cm)# create volume
-orb.PlaceIt(orbPos) # add volume to space
+GC.ConstructOrb("Orb", nai, [10., 10., 10.], cm, 10.)
 
-tube = G4EzVolume("Tube") # initialize volume
-tubePos = G4ThreeVector(-1., -1., -1.) # specify xyz location of volume
-tube.CreateTubeVolume(au, 30., 10.*cm, 30.*cm) # create volume
-tube.PlaceIt(tubePos) # add volume to space
+GC.ConstructTube("Tube", au, [-1., -1., -1.], cm, 10., 30., 30., 0, 300.)
+
+GC.ConstructSphere("Sphere", au, [0., 0., 0.], cm, 0., 10., 0., 300., 0., 150)
+
+GC.ConstructCone("Cone", nai, [-20., -20., -20.], cm, 0., 20., 0., 0., 25., 0., 180) # dphi = 359.9999 is basically 360, but we can still see it
+# sphere = G4EzVolume("Sphere")
+# spherePos = G4ThreeVector(1., 1., 1.)
+# sphere.CreateShpereVolume(au, 1.*cm, .5*m) # haha "Shpere" not "Sphere"
+# sphere.PlaceIt(spherePos)
 
 gRunManager.Initialize()
 
