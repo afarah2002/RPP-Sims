52385258556a2e9b2ef45b3ca869b450279e50a6
Albert Farah <arfarah2002@gmail.com>
2019-10-12 23:45:32 -0400

    applying mag field localizes randomized beam

--- GEANT4/beam2.py
+++ GEANT4/beam2.py
@@ -12,13 +12,24 @@ class MyPrimaryGeneratorAction(G4VUserPrimaryGeneratorAction):
 		print("Particle gun defined \n")
 
 	def GeneratePrimaries(self, event):
-		
+		# Magnetic field param
+		#################################################
+		magUnit = tesla
+		magVectorList = [[10,10,10],[-1,-1,-1]]
+		for vector in magVectorList:
+			fieldMgr = gTransportationManager.GetFieldManager()
+			myField = G4UniformMagField(G4ThreeVector(vector[0]*magUnit, vector[1]*magUnit, vector[2]*magUnit))
+			#myField= MyField()
+			fieldMgr.SetDetectorField(myField)
+			fieldMgr.CreateChordFinder(myField)
+		#################################################
+
+		# Particle param
+		#################################################
 		spaceParamDict = {}
-		vectorCount = 10
+		vectorCount = 100
 		locationArray = [0, 0, 0]
 
-
-		# ---- particle paramteters ---- #
 		particle = "e+"
 
 		energy_1 = 50	
@@ -30,20 +41,15 @@ class MyPrimaryGeneratorAction(G4VUserPrimaryGeneratorAction):
 		self.particleGun.SetParticleByName(particle) # define particle
 		self.particleGun.SetParticleEnergy(energy_1*energyUnit) # define particle energy 
 
-		for i in range(0,vectorCount):
+		for i in range(0,vectorCount): # creates random momentum vectors originating from [0, 0, 0]
 			px = random.uniform(-1,1)
 			py = random.uniform(-1,1)
 			pz = random.uniform(-1,1)
 			momentumArray = [px, py, pz]
-			# for locationArray, momentumArray in spaceParamDict.items(): # iterates to create as many particle guns as listed in spaceParamDict
 			self.particleGun.SetParticlePosition(G4ThreeVector(locationArray[0], locationArray[1], locationArray[2])*dimensionUnit) # define first particle generator location
 			self.particleGun.SetParticleMomentumDirection(G4ThreeVector(momentumArray[0], momentumArray[1], momentumArray[2])*dimensionUnit) # define first particle generator momentum
 			self.particleGun.GeneratePrimaryVertex(event)
-
-
-
-		
-
+		#################################################
 
 #-------------------------------------------------------------------
 class MyRunAction(G4UserRunAction):
